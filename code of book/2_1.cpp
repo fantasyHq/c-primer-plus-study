0ceb3a3e5935cd6ea8b59f41ebd4f68fa0fa3f79
fantasyHq <903626545@qq.com>
2019-12-07 15:43:53 +0800

    Create 2_1.cpp

---
+++ code of book/2_1.cpp
@@ -0,0 +1,11 @@
+//myfirst.cpp --displays a message
+
+#include <iostream>
+int main()
+{
+  using namespace std;
+  cout << "Come up and C++ ME SOME TIME.";
+  cout << endl;
+  cout << "You won't regret it!" <<endl;
+  return 0;
+}
